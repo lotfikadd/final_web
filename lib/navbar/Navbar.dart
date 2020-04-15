cc944e14fd42f718a96141f1031bd7e05df3ae93
lotfi_kadd <you@example.com>
2020-04-15 21:59:10 +0100

    new commit

--- lib/navbar/Navbar.dart
+++ lib/navbar/Navbar.dart
@@ -2,7 +2,6 @@ import 'package:final_web/ui/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
